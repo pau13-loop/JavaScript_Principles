f433ea28ee5f2be687c1ea956da40694660392e1
pau13-loop <pllinas@cifpfbmoll.eu>
2021-11-06 17:47:23 +0100

    feat: :sparkles: Add property_descriptor_03.js exercise

---
+++ objects/properties/descriptor/property_descriptor_03.js
@@ -0,0 +1,59 @@
+/**
+ * Property attributes and descriptor object
+ * 
+ * To set the attributes of a property, or to create a new property
+ * with the specified attributes, call Object.defineProperty(), 
+ * passing the object to be modified, the name of the property 
+ * to be created or altered, and the property descriptor object
+ * 
+ * JavaScript. the Definitive Guide. David Flanagan.
+ */
+
+//* Define more than one property simultaneously for the specified obj
+
+
+var o = Object.defineProperties({}, {
+    x: { value: 1, writable: true, enumerable: true, configurable: false },
+    y: { value: 1, writable: true, enumerable: true, configurable: true },
+    // z no es enumerable
+    z: { value: 1, writable: true, enumerable: false, configurable: true },
+    r: {
+        get: function () { return Math.sqrt(this.x * this.x + this.y * this.y) },
+        enumerable: true,
+        configurable: true
+    }
+});
+
+// z no es numerable
+for (property in o) {
+    console.log(property + " : " + o[property]);
+};
+
+// x es writable aunque no sea configurable
+o.x = 2;
+console.log("x write a 2: " + o.x);
+
+// cambio x a read only aunque no sea configurable
+Object.defineProperty(o, "x", { writable: false });
+o.x = 3;
+console.log("x writable false: " + o.x);    //* How we set the attribute "writeable" of "x" to false now we can not change the value of the prop "x"  
+
+// si no es configurable no puedo cambiar a writable
+// Object.defineProperty(o, "x",  { writable: true });     // error: cannot redefine property
+
+// como x no es configurable, no puedo cambiar enumerable ni configurable
+// Object.defineProperty(o, "x", { enumerable: false });   // error: cannot refedine property
+// Object.defineProperty(o, "x", { configurable: false });   // error: cannot refedine property
+
+//? Whyu we can not change the attributes of the prop I understand it but why before we could change the attribute "writeable"of "x" to false ???
+
+// si accessor property es configurable puedo hacerla data
+Object.defineProperty(o, "r", { value: 1 });
+console.log("r es data property ahora: " + o.r);
+
+// si data property es configurable puedo hacerla accessor
+Object.defineProperty(o, "y", { get: function () { return 0; } });
+console.log("y es get: " + o.y);
+
+// si data property no es configurable, no puedo hacerla accessor
+// Object.defineProperty(o, "x", {get: function() { return 0; } } );   // error: cannot refedine property
